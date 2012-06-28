767bf024cdc4ffe0b8f16332eca3b76237155f5e
Ed Venator <esv@case.edu>
2012-06-28 11:46:01 -0400

    Added estop pdfs, listener script, xBee pdf.
    Also modified ROS setup script.

--- ros_setup.sh
+++ ros_setup.sh
@@ -2,10 +2,10 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu lucid main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install ros-fuerte-desktop-full ros-fuerte-laser-drivers ros-fuerte-joystick-drivers ros-fuerte-camera-drivers ros-fuerte-audio-common ros-fuerte-arm-navigation
+sudo apt-get install ros-fuerte-desktop-full ros-fuerte-laser-drivers ros-fuerte-joystick-drivers ros-fuerte-camera-drivers ros-fuerte-audio-common ros-fuerte-arm-navigation ros-fuerte-arm-navigation-experimental
 mkdir -p ~/ros/dev_stacks
 echo 'source /opt/ros/fuerte/setup.bash
-export ROS_PACKAGE_PATH="/home/abby/ros/dev_stacks:$ROS_PACKAGE_PATH\' >> ~/.bashrc
+export ROS_PACKAGE_PATH="~/ros/dev_stacks:$ROS_PACKAGE_PATH\"' >> ~/.bashrc
 source ~/.bashrc
 sudo apt-get install python-setuptools python-pip
 sudo pip install -U rosinstall vcstools rosdep
